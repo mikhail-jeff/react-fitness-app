e5edc49b6e604d7b96a518cb03ccb9f2aca7f550
jeffmikhail <mikhailcruz78@gmail.com>
2023-01-19 22:58:47 +0800

    working on Exercise Details Page

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const ExerciseDetails = () => {
-	return <div>ExerciseDetails</div>;
+	return (
+		<Box>
+			<Detail />
+			<ExerciseVideos />
+			<SimilarExercises />
+		</Box>
+	);
 };
 
 export default ExerciseDetails;
